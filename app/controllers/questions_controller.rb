09bd233da9634405a731c4c4832027c842d7766a
Emma A. A. Rünzel <emma@ruenzel.de>
2023-08-14 12:31:24 +0200

    Add Controller & Views

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask;end
+
+  def answer
+    @question = params[:question]
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question.includ?("i am going to work".downcase)
+      @answer = "Great!"
+    elsif @question.ends_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
